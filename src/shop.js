aa73cd3048e75d98222da567f4d2a4b072f15ecf
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-06 16:38:24 -0700

    added jsx and deleted link routers

--- src/shop.js
+++ src/shop.js
@@ -1,24 +1,111 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-// , {useState}
-
+import {useHistory} from 'react-router-dom';
+import Cart from './cart.js';
 function Shop() {
+  let history = useHistory();
   return (
-    <div>
+    <div className="shop">
+      <Cart />
       <nav>
-        <div>
-          <span>
-            <Link to="/shop" style={{textDecoration: 'none', color: 'white'}}>
-              Shop
-            </Link>
+        <div
+          class="logo logo_shop"
+          onClick={() => {
+            history.push('/home');
+          }}
+        >
+          iFix
+        </div>
+        <div id="nav_right">
+          <span
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Shop
           </span>
-          <span>
-            <Link to="/cart" style={{textDecoration: 'none', color: 'white'}}>
-              Cart
-            </Link>
+          <span
+            onClick={() => {
+              document.getElementById('cart').style.display = 'flex';
+            }}
+          >
+            Cart
           </span>
         </div>
       </nav>
+      <main>
+        <aside>
+          <h1>Products</h1>
+          <span>Screens</span>
+          <span>Batteries</span>
+          <span>Data Ports</span>
+          <span>Cameras</span>
+          <span>Repair Kits</span>
+        </aside>
+        <section>
+          <div className="product">
+            <img src="images/iphone11.png" alt="Iphone 11" />
+            <span>Iphone 11 Screen</span>
+            <span>$150</span>
+          </div>
+          <div className="product">
+            <img src="./images/iphone11pro.jpeg" alt="Iphone 11 pro" />
+            <span>Iphone 11 Pro Screen</span>
+            <span>$200</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphone11promax.jpeg" alt="Iphone 11 Pro Max" />
+            <span>Iphone 11 Pro Max Screen</span>
+            <span>$250</span>
+          </div>
+
+          <div className="product">
+            <img src="images/iphonexsmax.jpeg" alt="Iphone XS Max" />
+            <span>Iphone XS Max Screen</span>
+            <span>$200</span>
+          </div>
+          <div className="product">
+            <img src="./images/iphonexs.jpeg" alt="Iphone xs" />
+            <span>Iphone Xs Screen</span>
+            <span>$180</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphonexr.jpeg" alt="Iphone XR" />
+            <span>Iphone XR Screen</span>
+            <span>$140</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphonex.jpeg" alt="Iphone X" />
+            <span>Iphone X Screen</span>
+            <span>$140</span>
+          </div>
+
+          <div className="product">
+            <img src="images/iphone12.jpeg" alt="Iphone 12" />
+            <span>Iphone 12 Screen</span>
+            <span>$200</span>
+          </div>
+          <div className="product">
+            <img src="./images/iphone12pro.jpeg" alt="Iphone 12 pro" />
+            <span>Iphone 12 Pro Screen</span>
+            <span>$180</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphone12promax.jpeg" alt="Iphone 12 Pro Max" />
+            <span>Iphone 12 Pro max Screen</span>
+            <span>$140</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphone12mini.jpeg" alt="Iphone 12 Mini" />
+            <span>Iphone 12 Mini Screen</span>
+            <span>$140</span>
+          </div>
+          <div className="product">
+            <img src="/images/iphonese.jpeg" alt="Iphone SE" />
+            <span>Iphone SE Screen</span>
+            <span>$140</span>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
