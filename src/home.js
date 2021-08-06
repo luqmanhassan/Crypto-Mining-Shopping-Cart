4585ee67eaef09742f681ba52282d0cb41c5dfe9
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-06 16:36:58 -0700

    Deleted Link routes and implemented useHistory

--- src/home.js
+++ src/home.js
@@ -1,30 +1,47 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
+import Cart from './cart.js';
 
 const Home = () => {
+  let history = useHistory();
   return (
     <div className="home">
+      <Cart />
       <nav>
-        <div id="logo">iFix</div>
+        <div
+          className="logo"
+          onClick={() => {
+            history.push('/home');
+          }}
+        >
+          iFix
+        </div>
         <div id="nav_right">
-          <span>
-            <Link to="/shop" style={{textDecoration: 'none', color: 'white'}}>
-              Shop
-            </Link>
+          <span
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Shop
           </span>
-          <span>
-            <Link to="/cart" style={{textDecoration: 'none', color: 'white'}}>
-              Cart
-            </Link>
+          <span
+            onClick={() => {
+              document.getElementById('cart').style.display = 'flex';
+            }}
+          >
+            Cart
           </span>
         </div>
       </nav>
+
       <main>
         <h1>Servicing your Electronic Repair Needs</h1>
-        <button>
-          <Link to="/shop" style={{textDecoration: 'none', color: 'black'}}>
-            Shop Now
-          </Link>
+        <button
+          onClick={() => {
+            history.push('/shop');
+          }}
+        >
+          Shop Now
         </button>
       </main>
     </div>
