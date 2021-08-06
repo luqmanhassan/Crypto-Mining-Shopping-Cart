1446899b61a01a90322865f9ffaefa1e30fbd3c0
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-06 16:35:53 -0700

    Made a working structure cart

--- src/cart.js
+++ src/cart.js
@@ -1,9 +1,26 @@
 import React from 'react';
-// , {useState}
+import {useHistory} from 'react-router-dom';
 const Cart = () => {
+  let history = useHistory();
   return (
-    <div>
-      <h1>Hello From Cart</h1>
+    <div className="cart" id="cart">
+      <img
+        src="./images/close.png"
+        alt="close icon"
+        onClick={() => {
+          document.getElementById('cart').style.display = 'none';
+        }}
+      />
+      <h1>Your Shopping Bag</h1>
+      <p>Your bag is empty.</p>
+      <button
+        onClick={() => {
+          history.push('/shop');
+          document.getElementById('cart').style.display = 'none';
+        }}
+      >
+        Browse Products
+      </button>
     </div>
   );
 };
