2a862a3df5f2731679891084788d134299c14876
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-07 17:14:58 -0700

    Cleared Home & Cart comp and added unit comp

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import App from './App.js';
-import Home from './home.js';
 import Shop from './shop.js';
-import Cart from './cart.js';
+import Unit from './unit.js';
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={App} />
-        <Route path="/home" exact component={Home} />
         <Route path="/shop" exact component={Shop} />
-        <Route path="/cart" exact component={Cart} />
+        <Route path="/unit" exact component={Unit} />
         <Route path="/" render={() => <div>404</div>} />
       </Switch>
     </BrowserRouter>
