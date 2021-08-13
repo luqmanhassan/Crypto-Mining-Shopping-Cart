be662f4ac2aebf994feeca1de5805ef21aadd25a
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-13 16:22:11 -0700

    Added Product Unit Comps

---
+++ src/unit1.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import iphone11 from './images/iphone11.png';
+
+const Unit1 = (props) => {
+  let history = useHistory();
+  function addtocart() {
+    document.getElementById('cart').style.display = 'flex';
+    document.getElementById('emptybag').style.display = 'none';
+    document.getElementById('cart_stuff').style.display = 'flex';
+    document.getElementById('checkout').style.display = 'block';
+    let x = document.getElementById('cart_items').innerHTML;
+    document.getElementsByClassName('cart_items').innerHTML = Number(x) + 1;
+  }
+  return (
+    <div className="Unit Unit1" id="Unit1">
+      <nav>
+        <div
+          class="logo logo_shop"
+          onClick={() => {
+            history.push('/');
+          }}
+        >
+          iFix
+        </div>
+        <div id="nav_right">
+          <span
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Shop
+          </span>
+          <span
+            onClick={() => {
+              document.getElementById('cart').style.display = 'flex';
+            }}
+          >
+            Cart
+          </span>
+          <span className="cart_items">0</span>
+        </div>
+      </nav>
+      <main>
+        <div className="thing">
+          <img src={iphone11} alt="Iphone 11 Screen" />
+          <span>Iphone 11 Screen</span>
+        </div>
+        <div className="info">
+          <span>$150</span>
+          <button onClick={addtocart}>Add To Cart</button>
+          <button
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Go Back
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Unit1;
---
+++ src/unit7.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import {useHistory} from 'react-router-dom';
+import xcamera from './images/xcamera.jpeg';
+const Unit7 = (props) => {
+  let history = useHistory();
+  function addtocart() {
+    document.getElementById('cart').style.display = 'flex';
+    document.getElementById('emptybag').style.display = 'none';
+    document.getElementById('cart_stuff').style.display = 'flex';
+    document.getElementById('checkout').style.display = 'block';
+    let x = document.getElementById('cart_items').innerHTML;
+    document.getElementsByClassName('cart_items').innerHTML = Number(x) + 1;
+  }
+  return (
+    <div className="Unit Unit7" id="Unit7">
+      <nav>
+        <div
+          class="logo logo_shop"
+          onClick={() => {
+            history.push('/');
+          }}
+        >
+          iFix
+        </div>
+        <div id="nav_right">
+          <span
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Shop
+          </span>
+          <span
+            onClick={() => {
+              document.getElementById('cart').style.display = 'flex';
+            }}
+          >
+            Cart
+          </span>
+          <span className="cart_items">0</span>
+        </div>
+      </nav>
+      <main>
+        <div className="thing">
+          <img src={xcamera} alt="Iphone X Camera" />
+          <span>Iphone X Camera </span>
+        </div>
+        <div className="info">
+          <span>$200</span>
+          <button onClick={addtocart}>Add To Cart</button>
+          <button
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Go Back
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Unit7;
