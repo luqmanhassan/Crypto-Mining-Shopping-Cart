d48c370ed9cf3299147d26a2286f549241ce0c9a
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-31 13:07:48 -0700

    Wrapped With Provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-
+import {Provider} from 'react-redux';
+import store from './store.js';
 ReactDOM.render(
   <React.StrictMode>
-    <Routes />
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
