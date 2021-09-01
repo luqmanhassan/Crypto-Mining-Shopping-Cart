a3275a74c543deda0b942f83a029432450948b4f
Luqman Hassan <reallookmanlook@gmail.com>
2021-08-31 17:54:00 -0700

    Included more props

--- src/item.js
+++ src/item.js
@@ -1,18 +1,45 @@
 import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
 
 const Item = (props) => {
+  const change = props.change;
+  const type1 = props.type1;
+  const type2 = props.type2;
+  const dispatch = useDispatch();
+  const _unit1 = useSelector((state) => state.unit1);
+
   return (
-    <div id="cart_stuff">
-      <img src={props.src} alt={props.alt} />
-      <div cart_stuff_inside>
-        <p>{props.category}</p>
+    <div id={props.id} className="cart_stuff">
+      <div id="stuff1">
+        <img src={props.src} alt={props.alt} />
         <p>{props.alt}</p>
         <p>{props.price}</p>
-        <div>
-          <span>-</span>
-          <input type="text" id="input_total" />
-          <span>+</span>
-        </div>
+      </div>
+
+      <div id="stuff2">
+        <span
+          onClick={() => {
+            dispatch({type: type1});
+          }}
+        >
+          -
+        </span>
+        <input
+          type="text"
+          id="input_total"
+          onChange={(ev) => {
+            dispatch({type: change, payload: ev.target.value});
+          }}
+        >
+          {_unit1}
+        </input>
+        <span
+          onClick={() => {
+            dispatch({type: type2});
+          }}
+        >
+          +
+        </span>
       </div>
     </div>
   );
