328c006978c006c870aeebeb8475684928ca485d
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-01 17:22:33 -0700

    Used Number method on state values

--- src/store.js
+++ src/store.js
@@ -14,10 +14,11 @@ function counterReducer(state = 0, action) {
       return state;
   }
 }
+
 function unit1Reducer(state = 0, action) {
   switch (action.type) {
     case 'unit1_add':
-      return state + 1;
+      return Number(state + 1);
     case 'unit1_minus':
       if (state > 0) {
         return state - 1;
@@ -25,97 +26,151 @@ function unit1Reducer(state = 0, action) {
         return state;
       }
     case 'unit1change':
-      return action.payload;
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit2Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit2_add':
+      return Number(state + 1);
+    case 'unit2_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit2change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit3Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit3_add':
+      return Number(state + 1);
+    case 'unit3_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit3change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit4Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit4_add':
+      return Number(state + 1);
+    case 'unit4_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit4change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit5Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit5_add':
+      return Number(state + 1);
+    case 'unit5_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit5change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit6Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit6_add':
+      return Number(state + 1);
+    case 'unit6_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit6change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit7Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit7_add':
+      return Number(state + 1);
+    case 'unit7_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit7change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit8Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit8_add':
+      return Number(state + 1);
+    case 'unit8_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit8change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit9Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit9_add':
+      return Number(state + 1);
+    case 'unit9_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit9change':
+      return Number(action.payload);
     default:
       return state;
   }
 }
 function unit10Reducer(state = 0, action) {
   switch (action.type) {
-    case 'add':
-      return state + 1;
-    case 'minus':
-      return state - 1;
+    case 'unit10_add':
+      return Number(state + 1);
+    case 'unit10_minus':
+      if (state > 0) {
+        return state - 1;
+      } else {
+        return state;
+      }
+    case 'unit10change':
+      return Number(action.payload);
     default:
       return state;
   }
