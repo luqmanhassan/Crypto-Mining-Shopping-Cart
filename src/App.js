447bc76ab4d96e4fe689ba38008722259ddac8f3
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-23 20:36:57 -0700

    edited props & css for mui comps

--- src/App.js
+++ src/App.js
@@ -3,25 +3,67 @@ import './index.css';
 import {useHistory} from 'react-router-dom';
 import Cart from './cart.js';
 import Nav from './nav.js';
-
+import {Grid, Button, Typography} from '@mui/material';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import image from './images/board.jpg';
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#31a0ff',
+    },
+    secondary: {
+      main: '#f8fdff',
+    },
+  },
+  typography: {
+    fontFamily: ['Times New Roman', 'Arial', 'sans-serif'].join(','),
+  },
+});
 const App = () => {
   let history = useHistory();
 
   return (
-    <div className="App">
-      <Cart />
-      <Nav />
-      <main>
-        <h1>Servicing your Electronic Repair Needs</h1>
-        <button
-          onClick={() => {
-            history.push('/shop');
-          }}
-        >
-          Shop Now
-        </button>
-      </main>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Grid
+        container
+        direction="column"
+        className="App"
+        sx={{
+          height: '750px',
+          backgroundImage: `url(${image})`,
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+        }}
+      >
+        <Cart />
+        <Nav home />
+
+        <Grid item align="center">
+          <Typography
+            variant="h2"
+            sx={{
+              mx: '300px',
+              textShadow: '0px 10px 5px black, -10px 5px 10px white',
+              color: 'secondary.main',
+              my: '50px',
+            }}
+          >
+            Electronic Repair Parts For Cheap
+          </Typography>
+        </Grid>
+        <Grid item align="center">
+          <Button
+            variant="contained"
+            sx={{fontSize: '25px', color: 'black', bgcolor: 'white'}}
+            onClick={() => {
+              history.push('/shop');
+            }}
+          >
+            Shop Now
+          </Button>
+        </Grid>
+      </Grid>
+    </ThemeProvider>
   );
 };
 
