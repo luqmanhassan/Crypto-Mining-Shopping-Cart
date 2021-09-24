adc8bc5af0d408ba077df05c405309ba1d0eafbb
Luqman Hassan <reallookmanlook@gmail.com>
2021-09-23 20:38:28 -0700

    edited comps & styles

--- src/shop.js
+++ src/shop.js
@@ -13,84 +13,100 @@ import xrdataport from './images/xrdataport.jpg';
 import xrcamera from './images/xrcamera.jpeg';
 import xcamera from './images/xcamera.jpeg';
 import Nav from './nav.js';
+import {Grid, Button, Typography} from '@mui/material';
+const selections = [
+  'Screens',
+  'Batteries',
+  'DataPorts',
+  'Cameras',
+  'RepairKits',
+];
+function toggle(select) {
+  for (let i = 0; i < selections.length; i++) {
+    if (selections[i] === select) {
+      document.getElementById(selections[i]).style.display = 'flex';
+    } else {
+      document.getElementById(selections[i]).style.display = 'none';
+    }
+  }
+}
 function Shop() {
   return (
-    <div className="shop">
+    <Grid container className="shop">
       <Cart />
       <Nav />
-      <main>
-        <aside>
-          <h1>Products</h1>
-          <span
+      <Grid item container sx={{display: 'flex', flexFlow: 'row nowrap'}}>
+        <Grid
+          item
+          sx={{
+            display: 'flex',
+            flexFlow: 'column nowrap',
+            p: '50px',
+          }}
+        >
+          <Typography variant="h3" pb="50px">
+            Products
+          </Typography>
+          <Button
             onClick={() => {
-              document.getElementById('Screens').style.display = 'flex';
-              document.getElementById('Batteries').style.display = 'none';
-              document.getElementById('DataPorts').style.display = 'none';
-              document.getElementById('Cameras').style.display = 'none';
-              document.getElementById('RepairKits').style.display = 'none';
+              toggle('Screens');
             }}
+            sx={{color: 'black'}}
           >
             Screens
-          </span>
-          <span
+          </Button>
+          <Button
             onClick={() => {
-              document.getElementById('Screens').style.display = 'none';
-              document.getElementById('Batteries').style.display = 'flex';
-              document.getElementById('DataPorts').style.display = 'none';
-              document.getElementById('Cameras').style.display = 'none';
-              document.getElementById('RepairKits').style.display = 'none';
+              toggle('Batteries');
             }}
+            sx={{color: 'black'}}
           >
             Batteries
-          </span>
-          <span
+          </Button>
+          <Button
             onClick={() => {
-              document.getElementById('Screens').style.display = 'none';
-              document.getElementById('Batteries').style.display = 'none';
-              document.getElementById('DataPorts').style.display = 'flex';
-              document.getElementById('Cameras').style.display = 'none';
-              document.getElementById('RepairKits').style.display = 'none';
+              toggle('DataPorts');
             }}
+            sx={{color: 'black'}}
           >
             Data Ports
-          </span>
-          <span
+          </Button>
+          <Button
             onClick={() => {
-              document.getElementById('Screens').style.display = 'none';
-              document.getElementById('Batteries').style.display = 'none';
-              document.getElementById('DataPorts').style.display = 'none';
-              document.getElementById('Cameras').style.display = 'flex';
-              document.getElementById('RepairKits').style.display = 'none';
+              toggle('Cameras');
             }}
+            sx={{color: 'black'}}
           >
             Cameras
-          </span>
-          <span
+          </Button>
+          <Button
             onClick={() => {
-              document.getElementById('Screens').style.display = 'none';
-              document.getElementById('Batteries').style.display = 'none';
-              document.getElementById('DataPorts').style.display = 'none';
-              document.getElementById('Cameras').style.display = 'none';
-              document.getElementById('RepairKits').style.display = 'flex';
+              toggle('RepairKits');
             }}
+            sx={{color: 'black'}}
           >
             Repair Kits
-          </span>
-        </aside>
+          </Button>
+        </Grid>
 
         <Screens />
         <Batteries />
         <DataPorts />
         <Cameras />
         <RepairKits />
-      </main>
-    </div>
+      </Grid>
+    </Grid>
   );
 }
 
 const Screens = () => {
   return (
-    <div className="Screens" id="Screens">
+    <Grid
+      item
+      className="Screens"
+      id="Screens"
+      sx={{display: 'flex', pt: '100px', ml: '100px'}}
+    >
       <Product
         src={iphone11}
         alt="Iphone 11 Screen"
@@ -104,13 +120,19 @@ const Screens = () => {
         unit="Unit2"
         price="150"
       />
-    </div>
+    </Grid>
   );
 };
 
 const Batteries = () => {
   return (
-    <div className="Batteries" id="Batteries">
+    <Grid
+      item
+      container
+      className="Batteries"
+      id="Batteries"
+      sx={{display: 'none', pt: '100px', ml: '100px'}}
+    >
       <Product src={xbattery} alt="Iphone X Battery" unit="Unit3" price="60" />
       <Product
         src={xrbattery}
@@ -118,13 +140,19 @@ const Batteries = () => {
         unit="Unit4"
         price="60"
       />
-    </div>
+    </Grid>
   );
 };
 
 const DataPorts = () => {
   return (
-    <div className="DataPorts" id="DataPorts">
+    <Grid
+      item
+      container
+      className="DataPorts"
+      id="DataPorts"
+      sx={{display: 'none', pt: '100px', ml: '100px'}}
+    >
       <Product
         src={xdataport}
         alt="Iphone X Data Port"
@@ -137,42 +165,58 @@ const DataPorts = () => {
         unit="Unit6"
         price="100"
       />
-    </div>
+    </Grid>
   );
 };
 
 const Cameras = () => {
   return (
-    <div className="Cameras" id="Cameras">
+    <Grid
+      item
+      container
+      className="Cameras"
+      id="Cameras"
+      sx={{display: 'none', pt: '100px', ml: '100px'}}
+    >
       <Product src={xcamera} alt="Iphone X Camera" unit="Unit7" price="130" />
       <Product src={xrcamera} alt="Iphone XR Camera" unit="Unit8" price="130" />
-    </div>
+    </Grid>
   );
 };
 
 const RepairKits = () => {
   return (
-    <div className="RepairKits" id="RepairKits">
+    <Grid
+      item
+      container
+      className="RepairKits"
+      id="RepairKits"
+      sx={{display: 'none', pt: '100px', ml: '100px'}}
+    >
       <Product src={kit1} alt="Iphone X Repair Kit" unit="Unit9" price="50" />
       <Product src={kit2} alt="Iphone XR Repair Kit" unit="Unit10" price="50" />
-    </div>
+    </Grid>
   );
 };
 
 const Product = (props) => {
   let history = useHistory();
   return (
-    <div
+    <Grid
+      item
       className="Product"
       id="Product"
       onClick={() => {
         history.push(props.unit);
       }}
+      direction="center"
+      align="center"
+      mx="10px"
     >
-      <img src={props.src} alt={props.alt} />
-      <span>{props.alt}</span>
-      <span>{props.price}</span>
-    </div>
+      <img src={props.src} alt={props.alt} width="300px" />
+      <Typography variant="h6">{props.alt}</Typography>
+      <Typography variant="body1">{props.price}</Typography>
+    </Grid>
   );
 };
 
